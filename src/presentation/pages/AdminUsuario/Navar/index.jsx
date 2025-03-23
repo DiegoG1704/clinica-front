ae5c0e4bb8db425686b7eb37d65394069a58a546
diego_ibarra <di74310048@gmail.com>
2025-03-23 04:10:34 -0500

    add  all items and update iu in sidebar

--- src/presentation/pages/AdminUsuario/Navar/index.jsx
+++ src/presentation/pages/AdminUsuario/Navar/index.jsx
@@ -6,6 +6,8 @@ import 'primeicons/primeicons.css';
 import { useNavigate } from 'react-router-dom';
 import fotoperfil from "../../../img/photo-default.png";
 import { useAuth } from '../../../context/AuthContext/AuthContext';
+import LogoImage from "@/presentation/img/logo-inicio.png";
+import { Divider } from 'primereact/divider';
 
 export default function Sidebar({ isOpen, toggleSidebar, onLogout }) {
   const navigate = useNavigate();
@@ -26,35 +28,62 @@ export default function Sidebar({ isOpen, toggleSidebar, onLogout }) {
   return (
     <>
       <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
-        <h1><span className='sidebar__logo-icon'>|</span>MAS SALUD</h1>
-        <div className="flex  justify-content-center ">
-          <img src={user.fotoPerfil ? `${process.env.REACT_APP_API_BASE_URL}uploads/${user.fotoPerfil}` : fotoperfil} alt='' className='image-perfil' />
+        {/* <h1><span className='sidebar__logo-icon'>|</span>MAS SALUD</h1> */}
+        <div className="container-logo">
+          <img src={LogoImage} alt="" className='logo-massalud' />
         </div>
 
-        <div className="flex justify-content-center"><h2 className="user-name ">{user?.nombres}</h2></div>
+        <div className="flex  justify-content-center perfil relative   ">
+          <div className='divider '></div>
+          <div className='profile-avatar-container absolute  '>
+            <img src={user.fotoPerfil ? `${process.env.REACT_APP_API_BASE_URL}uploads/${user.fotoPerfil}` : fotoperfil} alt='' className='image-perfil ' />
+          </div>
 
-        <h3>{user?.rol}</h3>
+        </div>
+
+        <div className='container-user-info'>
+          <div className="flex justify-content-center"><h2 className="user-name ">{user?.nombres}</h2></div>
+
+          <div className='role'><div><h3>{user?.rol}</h3></div></div>
+          <div className='link-perfil'>
+            <div>
+              <i className="pi pi-cog" style={{ fontSize: '13px' }} />
+              <span style={{ fontSize: '14px' }}>Mi Perfil</span>
+            </div>
+
+          </div>
+        </div>
+        <div className='divider divider-links'></div>
         <ul className="sidebar-menu">
+          <p className='section-title-links'>MENÚ PRINCIPAL</p>
           {user.rutas.map((rut, index) =>
             rut.ruta !== '/RestrictedAccess' ? ( // Excluye solo la ruta deseada
               <li key={index} onClick={() => navigate(rut.ruta)}>
-                <i className={rut.logo} style={{ fontSize: '20px' }} />
+                <i className={rut.logo} style={{ fontSize: '13.7px' }} />
                 {isOpen && <span style={{ fontSize: '14px' }}>{rut?.nombre}</span>}
               </li>
             ) : <li key={index} onClick={() => navigate('/RestrictedAccess')}>
-              <i className={"pi pi-home"} style={{ fontSize: '20px' }} />
+              <i className={"pi pi-home"} style={{ fontSize: '13px' }} />
               {isOpen && <span style={{ fontSize: '14px' }}>Home</span>}
             </li>
           )}
+          <div className="divider"></div>
+          <p className='section-title-links'>CONFIGURACIÓN</p>
           <li onClick={() => navigate('/Configuraciones')}>
-            <i className="pi pi-cog" style={{ fontSize: '20px' }} />
+            <i className="pi pi-cog" style={{ fontSize: '13.7px' }} />
             {isOpen && <span style={{ fontSize: '14px' }}>Configuración</span>}
           </li>
-          <li onClick={handleLogout}>
-            <i className="pi pi-sign-out" style={{ fontSize: '20px' }} />
+          <div className="divider"></div>
+          <li onClick={handleLogout} className='close-session'>
+            <i className="pi pi-sign-out" style={{ fontSize: '13.7px' }} />
             {isOpen && <span style={{ fontSize: '14px' }}>Cerrar Sesión</span>}
           </li>
+
         </ul>
+        
+       
+
+
       </div>
       <Button
         rounded
